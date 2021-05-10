0eb9cede2e13e9f41d4a901fdb9d83c2507b59aa
Adam Kulesza <adam.kule@gmail.com>
2021-05-10 17:13:30 +0200

    Volley req works

--- app/src/main/java/com/example/bikenavigatorapp/Navigator.kt
+++ app/src/main/java/com/example/bikenavigatorapp/Navigator.kt
@@ -5,11 +5,13 @@ import android.util.Log
 import com.android.volley.*
 import com.android.volley.toolbox.HttpHeaderParser
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.gms.location.LocationServices
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.UnsupportedEncodingException
+
 
 class Navigator(private val context:Context) {
     private companion object{
@@ -17,61 +19,26 @@ class Navigator(private val context:Context) {
         const val TAG = "Navigator";
     }
 
-    private val geofencingClient by lazy { LocationServices.getGeofencingClient(context) }
-    private val queue: RequestQueue = Volley.newRequestQueue(context)
+    private fun getUrlParams():String{
+        return "origin=52.11479%2C21.00070&destination=52.235782%2C20.940026&mode=bicycling&key=${BuildConfig.DIR_API_KEY}"
+    }
 
-    private val navRequest = object : JsonObjectRequest(
-        Method.GET, DIR_API_URL, JSONObject(mapOf(
-            "origin" to "52.11479,21.00070",
-            "destination" to "52.235782,20.940026",
-            "mode" to "bicycling",
-            "key" to BuildConfig.DIR_API_KEY
-        )),
+    private val navRequest = JsonObjectRequest(
+        Request.Method.GET, "$DIR_API_URL?${getUrlParams()}", null,
         {
             Log.i(TAG,"Directions API returned: $it")
         },
         { error ->
             error.networkResponse.let {
-                Log.w(TAG,"Error while requesting new directions: code=${it.statusCode} msg=${String(it.data,kotlin.text.Charsets.UTF_8)}")
+                Log.w(TAG,"Error while requesting new directions: code=${it.statusCode} msg=${String(it.data,Charsets.UTF_8)}")
             }
         }
-    ) {
-        override fun getParams(): Map<String, String>{
-            return mapOf(
-                "origin" to "52.11479,21.00070",
-                "destination" to "52.235782,20.940026",
-                "mode" to "bicycling",
-                "key" to BuildConfig.DIR_API_KEY
-            )
-        }
-    }
+    )
+    private val queue: RequestQueue = Volley.newRequestQueue(context)
+
+    private val geofencingClient by lazy { LocationServices.getGeofencingClient(context) }
 
     fun updateDirections(){
-        val req = VolleyJsonRequest(
-            Request.Method.GET,
-            DIR_API_URL,
-            mapOf(
-                "origin" to "52.11479,21.00070",
-                "destination" to "52.235782,20.940026",
-                "mode" to "bicycling",
-                "key" to BuildConfig.DIR_API_KEY
-            ), Response.Listener {
-                Log.i(TAG,"Directions API returned: $it")
-            },Response.ErrorListener { error ->
-                error.networkResponse.let {
-                    Log.w(
-                        TAG,
-                        "Error while requesting new directions: code=${it.statusCode} msg=${
-                            String(
-                                it.data,
-                                kotlin.text.Charsets.UTF_8
-                            )
-                        }"
-                    )
-                }
-            }
-        )
-        req.setShouldCache(false)
-        queue.add(req)
+        queue.add(navRequest)
     }
 }
