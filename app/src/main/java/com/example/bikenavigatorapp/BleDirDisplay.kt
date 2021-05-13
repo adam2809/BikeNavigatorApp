9fc5af3f19dc338cdeef41bab0ad89fee459c71e
root <iamanidiot@runninggit.asroot>
2021-05-13 15:24:46 +0200

    Extracts ble display functionality to separate class

---
+++ app/src/main/java/com/example/bikenavigatorapp/BleDirDisplay.kt
@@ -0,0 +1,175 @@
+package com.example.bikenavigatorapp
+
+import android.Manifest
+import android.bluetooth.*
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.os.Build
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
+import java.util.*
+
+class BleDirDisplay(private val context: MainActivity) {
+    private companion object {
+        const val TAG = "BleDirDisplay"
+        const val SCAN_PERIOD: Long = 2000;
+        const val DEVICE_ADDRESS = "7C:9E:BD:06:E4:AA";
+        const val DISPLAY_SERVICE_UUID = "000000ff-0000-1000-8000-00805f9b34fb";
+        const val DISPLAY_CHARACTERISTIC_UUID = "0000ff01-0000-1000-8000-00805f9b34fb";
+    }
+
+    enum class Dir {
+        NO_DIR,
+        RIGHT,
+        STRAIGHT,
+        LEFT
+    }
+
+    private val bluetoothManager by lazy { getSystemService(context, BluetoothManager::class.java) }
+    private val bluetoothAdapter by lazy { bluetoothManager!!.adapter }
+    private val bluetoothLeScanner by lazy { bluetoothAdapter.bluetoothLeScanner }
+
+    private var scanning = false
+    private val handler = Handler()
+
+    var bluetoothGatt: BluetoothGatt? = null
+    var displayCharacteristic: BluetoothGattCharacteristic? = null
+
+    private val bluetoothGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(
+            gatt: BluetoothGatt, status: Int,
+            newState: Int
+        ) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "Connected to GATT server.")
+
+                gatt.discoverServices()
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "Disconnected from GATT server.")
+            }
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+            super.onServicesDiscovered(gatt, status)
+            displayCharacteristic = findCharacteristic(gatt).apply {
+                if (this != null) {
+                    Log.i(TAG, "Found characteristic $this")
+                } else {
+                    Log.e(TAG, "Characteristic was not found")
+                }
+            };
+
+        }
+    }
+
+
+    fun initiateScan() {
+        if (!context.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(
+                context,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                context.LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+        scanLeDevice { res ->
+            bluetoothGatt = res.device.connectGatt(context, false, bluetoothGattCallback)
+        }
+    }
+
+    private fun scanLeDevice(ifFoundCb: (ScanResult) -> Unit) {
+        var found: ScanResult? = null
+
+        val leScanCallback: ScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                Log.d(TAG, "New scan result device: $result");
+                if (result.device.address == DEVICE_ADDRESS) {
+                    found = result
+                    Log.i(TAG, "Found device: $result");
+                }
+            }
+        }
+
+        bluetoothLeScanner.let { scanner ->
+            if (!scanning) {
+                handler.postDelayed({
+                    Log.i(TAG, "Stopping scan (handler)");
+                    scanning = false
+                    scanner.stopScan(leScanCallback)
+                    found?.let {
+                        Log.i(TAG, "Found $found");
+                        ifFoundCb(it);
+                    }
+                }, SCAN_PERIOD)
+                Log.i(TAG, "Starting scan for ${SCAN_PERIOD} ms");
+                scanning = true
+                scanner.startScan(leScanCallback)
+            }
+        }
+    }
+
+
+    private fun findCharacteristic(gatt: BluetoothGatt?): BluetoothGattCharacteristic? {
+        val found = gatt!!.services.let {
+            it.filter { service ->
+                service.also {
+                    Log.d(
+                        TAG,
+                        "Service in gatt discovered: ${service.uuid}"
+                    )
+                }.uuid == UUID.fromString(DISPLAY_SERVICE_UUID);
+            }
+        }.flatMap {
+            it.characteristics
+        }.let {
+            it.filter { characteristic ->
+                characteristic.also {
+                    Log.d(
+                        TAG,
+                        "Characteristic in gatt discovered: ${characteristic.uuid}"
+                    )
+                }.uuid == UUID.fromString(DISPLAY_CHARACTERISTIC_UUID)
+            }
+        }
+        return found.elementAtOrNull(0);
+    }
+
+    private fun isBtDeviceReadyForAccess(): Boolean {
+        return displayCharacteristic != null // && bluetoothManager?.getConnectionState(bluetoothGatt?.device) == BluetoothProfile.STATE_CONNECTED
+    }
+
+    fun writeDir(dir: Dir) {
+        if (!isBtDeviceReadyForAccess()) {
+            Log.w(TAG, "Attempting to access device which is not ready")
+            return
+        }
+        bluetoothGatt?.let { gatt ->
+            displayCharacteristic?.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+            displayCharacteristic?.value = ByteArray(1) { dir.ordinal.toByte() }
+            gatt.writeCharacteristic(displayCharacteristic)
+        } ?: Log.e(TAG, "Unable to write straight")
+    }
+
+    fun straight() {
+        writeDir(Dir.STRAIGHT)
+    }
+
+    fun left() {
+        writeDir(Dir.LEFT)
+
+    }
+
+    fun right() {
+        writeDir(Dir.RIGHT)
+    }
+
+    fun noDir() {
+        writeDir(Dir.NO_DIR)
+    }
+
+    fun isBtEnabled(): Boolean = bluetoothAdapter.isEnabled
+}
--- app/src/main/java/com/example/bikenavigatorapp/MainActivity.kt
+++ app/src/main/java/com/example/bikenavigatorapp/MainActivity.kt
@@ -4,10 +4,6 @@ import android.Manifest
 import android.app.Activity
 import android.app.PendingIntent
 import android.bluetooth.*
-import android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanResult
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.*
@@ -20,18 +16,9 @@ import com.example.bikenavigatorapp.geofencing.GeofenceBroadcastReceiver
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingRequest
 import com.google.android.gms.location.LocationServices
-import java.util.*
 
-
-fun Context.hasPermission(permissionType: String): Boolean {
-    return ContextCompat.checkSelfPermission(this, permissionType) ==
-            PackageManager.PERMISSION_GRANTED
-}
 
 class MainActivity : AppCompatActivity() {
-    private val bluetoothManager by lazy { getSystemService(BluetoothManager::class.java) }
-    private val bluetoothAdapter by lazy { bluetoothManager.adapter }
-    private val bluetoothLeScanner by lazy { bluetoothAdapter.bluetoothLeScanner }
     private val geofencingClient by lazy { LocationServices.getGeofencingClient(this) }
     private val geofencePendingIntent: PendingIntent by lazy {
         val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
@@ -43,49 +30,13 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private var scanning = false
-    private val handler = Handler()
+    private val dirDisplay = BleDirDisplay(this)
 
-    var bluetoothGatt: BluetoothGatt? = null
-    var displayCharacteristic:BluetoothGattCharacteristic? = null
+    private val TAG = "MainActivity";
+    private val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+    val LOCATION_PERMISSION_REQUEST_CODE = 2
+    private val GEOFENCE_REQ_IDS_START = 100
 
-    private val bluetoothGattCallback = object : BluetoothGattCallback() {
-        override fun onConnectionStateChange(
-            gatt: BluetoothGatt, status: Int,
-            newState: Int
-        ) {
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                Log.i(TAG, "Connected to GATT server.")
-
-                gatt.discoverServices()
-            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                Log.i(TAG, "Disconnected from GATT server.")
-            }
-        }
-
-        override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-            super.onServicesDiscovered(gatt, status)
-            displayCharacteristic = findCharacteristic(gatt).apply {
-                if (this!=null){
-                    Log.i(TAG,"Found characteristic $this")
-                }else{
-                    Log.e(TAG,"Characteristic was not found")
-                }
-            };
-
-        }
-    }
-
-    private companion object {
-        const val SCAN_PERIOD: Long = 2000;
-        const val TAG = "MainActivity";
-        const val DEVICE_ADDRESS = "7C:9E:BD:06:E4:AA";
-        const val DISPLAY_SERVICE_UUID = "000000ff-0000-1000-8000-00805f9b34fb";
-        const val DISPLAY_CHARACTERISTIC_UUID = "0000ff01-0000-1000-8000-00805f9b34fb";
-        const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
-        const val LOCATION_PERMISSION_REQUEST_CODE = 2
-        const val GEOFENCE_REQ_IDS_START = 100
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -94,13 +45,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        if (!bluetoothAdapter.isEnabled) {
-            promptEnableBluetooth()
-        }
+        promptEnableBluetooth()
     }
 
     private fun promptEnableBluetooth() {
-        if (!bluetoothAdapter.isEnabled) {
+        if (!dirDisplay.isBtEnabled()) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
         }
@@ -132,99 +81,28 @@ class MainActivity : AppCompatActivity() {
                         LOCATION_PERMISSION_REQUEST_CODE
                     )
                 } else {
-                    initiateScan()
+                    dirDisplay.initiateScan()
                 }
             }
         }
     }
 
-    private fun scanLeDevice(ifFoundCb:(ScanResult) -> Unit) {
-        var found:ScanResult? = null
 
-        val leScanCallback: ScanCallback = object : ScanCallback() {
-            override fun onScanResult(callbackType: Int, result: ScanResult) {
-                super.onScanResult(callbackType, result)
-                Log.d(TAG, "New scan result device: $result");
-                if(result.device.address == DEVICE_ADDRESS){
-                    found = result
-                    Log.i(TAG, "Found device: $result");
-                }
-            }
-        }
-
-        bluetoothLeScanner.let { scanner ->
-            if (!scanning) {
-                handler.postDelayed({
-                    Log.i(TAG, "Stopping scan (handler)");
-                    scanning = false
-                    scanner.stopScan(leScanCallback)
-                    found?.let {
-                        Log.i(TAG, "Found $found");
-                        ifFoundCb(it);
-                    }
-                }, SCAN_PERIOD)
-                Log.i(TAG, "Starting scan for $SCAN_PERIOD ms");
-                scanning = true
-                scanner.startScan(leScanCallback)
-            }
-        }
+    fun blutacz(v: View) {
+        dirDisplay.initiateScan()
     }
 
-    fun blutacz(v: View){
-        initiateScan()
+    fun straight(v: View) {
+        dirDisplay.straight()
     }
 
-    private fun initiateScan(){
-        if(!hasPermission(Manifest.permission.ACCESS_FINE_LOCATION) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-        }
-        scanLeDevice { res ->
-            bluetoothGatt = res.device.connectGatt(this, false, bluetoothGattCallback)
-        }
+    fun left(v: View) {
+        dirDisplay.left()
+
     }
 
-    private fun isBtDeviceReadyForAccess():Boolean{
-        return displayCharacteristic != null // && bluetoothManager?.getConnectionState(bluetoothGatt?.device) == BluetoothProfile.STATE_CONNECTED
-    }
-
-    fun straight(v: View){
-        if(!isBtDeviceReadyForAccess()){
-            Log.w(TAG,"Attempting to access device which is not ready")
-            return
-        }
-        bluetoothGatt?.let { gatt ->
-            displayCharacteristic?.writeType = WRITE_TYPE_DEFAULT
-            displayCharacteristic?.value = ByteArray(1){ 2 }
-            gatt.writeCharacteristic(displayCharacteristic)
-        } ?: Log.e(TAG,"Unable to write straight")
-    }
-    fun left(v: View){
-        if(!isBtDeviceReadyForAccess()){
-            Log.w(TAG,"Attempting to access device which is not ready")
-            return
-        }
-
-        bluetoothGatt?.let { gatt ->
-            displayCharacteristic?.writeType = WRITE_TYPE_DEFAULT
-            displayCharacteristic?.value = ByteArray(1){ 1 }
-            gatt.writeCharacteristic(displayCharacteristic)
-        } ?: Log.e(TAG,"Unable to write straight")
-    }
-    fun right(v: View){
-        if(!isBtDeviceReadyForAccess()){
-            Log.w(TAG,"Attempting to access device which is not ready")
-            return
-        }
-
-        bluetoothGatt?.let { gatt ->
-            displayCharacteristic?.writeType = WRITE_TYPE_DEFAULT
-            displayCharacteristic?.value = ByteArray(1){ 3 }
-            gatt.writeCharacteristic(displayCharacteristic)
-        } ?: Log.e(TAG,"Unable to write straight")
+    fun right(v: View) {
+        dirDisplay.right()
     }
 
 
@@ -275,7 +153,7 @@ class MainActivity : AppCompatActivity() {
         geofencingClient
             .addGeofences(buildGeofencingRequest(geofences), geofencePendingIntent)
             .addOnSuccessListener {
-                Log.i(TAG,"Successfuly submitted geofences")
+                Log.i(TAG, "Successfully submitted geofences")
             }
             .addOnFailureListener {
                 Log.e(TAG,"Error while submitting geofences: ${it.message}")
@@ -287,17 +165,5 @@ class MainActivity : AppCompatActivity() {
             .setInitialTrigger(0)
             .addGeofences(geofences)
             .build()
-    }
-
-
-    private fun findCharacteristic(gatt:BluetoothGatt?):BluetoothGattCharacteristic?{
-        val found = gatt!!.services.let {
-            it.filter { service -> service.also { Log.d(TAG,"Service in gatt discovered: ${service.uuid}") }.uuid == UUID.fromString(DISPLAY_SERVICE_UUID); }
-        }.flatMap {
-            it.characteristics
-        }.let {
-            it.filter { characteristic -> characteristic.also { Log.d(TAG,"Characteristic in gatt discovered: ${characteristic.uuid}") }.uuid == UUID.fromString(DISPLAY_CHARACTERISTIC_UUID) }
-        }
-        return found.elementAtOrNull(0);
     }
 }
